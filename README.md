a6a94f1873c1b0ccc4fb850b8e368096f509f9d0
huiop368 <negativemh@gmail.com>
2017-03-20 18:57:33 +0800

    init file

---
+++ README.md
@@ -0,0 +1,7 @@
+## TitanUI 简单上手demo
+这是一个非常简单钛UI上手Demo, 里面有run项目的基础webpack配置
+
+1. `tnpm install` 
+2. `npm start`即可在本地通过http://0.0.0.0:8080访问
+
+大家可以在Demo里面体验钛UI~~
---
+++ webpack.config.js
@@ -0,0 +1,131 @@
+const webpack   = require('webpack');
+const path      = require('path');
+const merge     = require('webpack-merge');
+
+// Plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer      = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// Constans
+const ENV = process.env.NODE_ENV;
+const PATHS = {
+    dist: path.join(__dirname, 'build')
+};
+
+const common = {
+    entry : path.join(__dirname, 'src/main.js'),
+
+    output: {
+        path: PATHS.dist,
+        filename: 'bundle.js'
+    },
+
+    module : {
+      loaders : [
+        // 处理es6, babel配置请查看.babelrc
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel'
+        },
+        // 处理iconfont
+        { test: /\.woff(\?.*)?$/,  loader: 'url?limit=10000&mimetype=application/font-woff' },
+        { test: /\.woff2(\?.*)?$/, loader: 'url?limit=10000&mimetype=application/font-woff2' },
+        { test: /\.otf(\?.*)?$/,   loader: 'file?limit=10000&mimetype=font/opentype' },
+        { test: /\.ttf(\?.*)?$/,   loader: 'url?limit=10000&mimetype=application/octet-stream' },
+        { test: /\.eot(\?.*)?$/,   loader: 'file?limit=10000' },
+        { test: /\.svg(\?.*)?$/,   loader: 'url?limit=10000&mimetype=image/svg+xml' },
+        { test: /\.(png|jpg)$/,    loader: 'url?limit=8192' }
+      ]
+    },
+
+    // css 前缀处理
+    postcss: [ autoprefixer({ browsers: ['>0.5%','last 2 versions'] }) ],
+
+    resolve: {
+        extensions: ['', '.js', '.less'],
+        alias: {
+            
+        }
+    },
+
+    plugins : [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'index.html'),
+            hash: false,
+            filename: 'index.html',
+            inject: true,
+            minify: {
+              collapseWhitespace: true
+            }
+        })
+    ]
+};
+
+
+//@开发模式
+
+if( ENV === 'development' || !ENV){
+    module.exports = merge(common, {
+        module : {
+            loaders : [
+                {
+                    test : /\.css$/,
+                    //exclude : /node_modules/,
+                    loader : 'style!css!postcss'
+                }
+            ]
+        },
+
+        devtool: 'eval-source-map',
+
+        devServer : {
+            contentBase : PATHS.dist,
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+            // Display only errors to reduce the amount of output.
+            stats: 'errors-only',
+            host: process.env.HOST || '0.0.0.0',
+            port: process.env.PORT || '8080'
+        },
+
+        plugins : [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    })     
+}
+
+
+
+//@部署模式
+
+if( ENV === 'production' ){
+    module.exports = merge(common, {
+        module : {
+            loaders : [
+                {
+                    test : /\.css$/,
+                    //exclude : /node_modules/,
+                    loader: ExtractTextPlugin.extract('style', 'css!postcss')
+                }
+            ]
+        },
+
+        plugins : [
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            }),
+
+            new ExtractTextPlugin('[name].css'),
+
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': '"production"'
+            })
+        ]
+    })
+}
